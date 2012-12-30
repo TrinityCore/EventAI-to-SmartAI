cd468c63300f61998ff4b5881bbc86bcc00935e1
Warpten <vertozor@gmail.com>
2012-12-30 23:28:56 +0100

    Add DBC::getFieldFromRecord(recordId, fieldName, fieldType) to Tim Kurvers' DBC Library.

--- PHP/dep/lib/DBC.class.php
+++ PHP/dep/lib/DBC.class.php
@@ -2,26 +2,26 @@
 /**
  * World of Warcraft DBC Library
  * Copyright (c) 2011 Tim Kurvers <http://www.moonsphere.net>
- * 
+ *
  * This library allows creation, reading and export of World of Warcraft's
  * client-side database files. These so-called DBCs store information
  * required by the client to operate successfully and can be extracted
  * from the MPQ archives of the actual game client.
- * 
+ *
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * Alternatively, the contents of this file may be used under the terms of
  * the GNU General Public License version 3 license (the "GPLv3"), in which
  * case the provisions of the GPLv3 are applicable instead of the above.
- * 
+ *
  * @author	Tim Kurvers <tim@moonsphere.net>
  */
 
@@ -36,7 +36,7 @@ class DBC implements IteratorAggregate {
 	const SIGNATURE = 'WDBC';
 	
 	/**
-	 * Defines the size of the header in bytes 
+	 * Defines the size of the header in bytes
 	 */
 	const HEADER_SIZE = 20;
 	
@@ -369,6 +369,21 @@ class DBC implements IteratorAggregate {
 		}
 		return null;
 	}
+
+    /**
+     * Fetches a field from the DBC, given the record's ID (first field), and ensures the index is generated
+     */
+    public function getFieldFromRecord($recordId, $fieldName, $fieldType = DBC::UINT) {
+        if($this->_index === null)
+            $this->index();
+        if(isset($this->_index[$recordId])) {
+            $record = new DBCRecord($this, $this->_index[$recordId]);
+            $ret = $record->get($fieldName, $fieldType);
+            unset($record);
+            return $ret;
+        }
+        return null;
+    }
 	
 	/**
 	 * Whether this DBC has a record at the given zero-based position
